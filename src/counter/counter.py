02098b82f193d95d446efe373b92777cc1ff8f54
Coconut Aero <148566655+Coconut-Aero@users.noreply.github.com>
2024-12-11 19:50:23 +0800

    fix counter.kicad_sch counter.py

--- src/counter/counter.py
+++ src/counter/counter.py
@@ -9,7 +9,7 @@ def d1(d0_last_state, d1_last_state, x_input):
     return int(not(share_func.xor2(x_input, share_func.xor2(d0_last_state, d1_last_state))))
 
 def output(d0_last_state, d1_last_state, x_input):
-    return int(d0_last_state == 1 and d1_last_state == 1 and x_input == 1)
+    return int((d0_last_state == 1 and d1_last_state == 1 and x_input == 1)or(d0_last_state == 0 and d1_last_state == 0 and x_input == 0))
 
 input_x = np.array([0,0,0,0,0,1,1,1,1,1])
 output_q0 = []
@@ -24,11 +24,11 @@ for i in range(10):
     elif i<=4 or i>5:
         output_q0.append(d0(output_q0[i-1]))
         output_q1.append(d1(output_q0[i-1],output_q1[i-1],input_x[i]))
-        output_z.append(output(output_q0[i-1],output_q1[i-1],input_x[i]))
+        output_z.append(output(output_q0[i],output_q1[i],input_x[i]))
     elif i==5:
         output_q0.append(0)
         output_q1.append(0)
-        output_z.append(d1(output_q0[i-1],output_q1[i-1],input_x[i]))
+        output_z.append(d1(output_q0[i],output_q1[i],input_x[i]))
 
 
 steps = np.arange(len(input_x))
